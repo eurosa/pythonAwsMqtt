66539f465d56bd457e8c82fbcfe8478724a54404
kali <ranojank@timxn.com>
2023-04-12 17:28:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+import time as t
+import json
+import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
+
+# Define ENDPOINT, CLIENT_ID, PATH_TO_CERTIFICATE, PATH_TO_PRIVATE_KEY, PATH_TO_AMAZON_ROOT_CA_1, MESSAGE, TOPIC, and RANGE
+ENDPOINT = "customEndpointUrl"
+CLIENT_ID = "testDevice"
+PATH_TO_CERTIFICATE = "certificates/a1b23cd45e-certificate.pem.crt"
+PATH_TO_PRIVATE_KEY = "certificates/a1b23cd45e-private.pem.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "certificates/root.pem"
+MESSAGE = "Hello World"
+TOPIC = "test/testing"
+RANGE = 20
+
+myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
+myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
+myAWSIoTMQTTClient.configureCredentials(PATH_TO_AMAZON_ROOT_CA_1, PATH_TO_PRIVATE_KEY, PATH_TO_CERTIFICATE)
+
+myAWSIoTMQTTClient.connect()
+print('Begin Publish')
+for i in range (RANGE):
+    data = "{} [{}]".format(MESSAGE, i+1)
+    message = {"message" : data}
+    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1)
+    print("Published: '" + json.dumps(message) + "' to the topic: " + "'test/testing'")
+    t.sleep(0.1)
+print('Publish End')
+myAWSIoTMQTTClient.disconnect()
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
